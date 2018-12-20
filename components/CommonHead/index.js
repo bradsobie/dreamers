47d8a5f55b1b07f230b045f59db8cbdf0bb0842e
Brad Sobie <brad_sobie@volusion.com>
2018-12-20 17:08:36 -0600

    Moving web font styles to link tag for ssr

--- components/CommonHead/index.js
+++ components/CommonHead/index.js
@@ -5,5 +5,6 @@ export default () => (
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta name="robots" content="noindex" />
     <link rel="shortcut icon" type="image/png" href="/static/favicon.png"/>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet"></link>
   </Fragment>
 );
--- globalStyles.js
+++ globalStyles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700');
-
   body {
     margin: 0;
     font-family: 'Montserrat', sans-serif;
