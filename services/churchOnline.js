c313cc26477a8909420b228095a408e541e797b9
Bradley Sobie <bradsobie@gmail.com>
2019-01-01 00:59:06 -0500

    Using https endpoint

--- services/churchOnline.js
+++ services/churchOnline.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-const CHURCH_ONLINE_API_URL = 'http://dreamers-atx.churchonline.org/api/v1/events/current';
+const CHURCH_ONLINE_API_URL = 'https://dreamers-atx.churchonline.org/api/v1/events/current';
 export const getLiveInfo = () => axios.get(CHURCH_ONLINE_API_URL).then(response => response.data.response.item);
