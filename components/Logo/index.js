1c49f3dbe035356326dcbf3640b759ec08778c30
Bradley Sobie <bradsobie@gmail.com>
2018-12-31 22:49:46 -0500

    Adding dreamers logo

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -1,19 +1,21 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
-const Logo = styled.h1`
-  color: #fff;
-  font-size: 24px;
+const Logo = styled.img`
   position: absolute;
   top: 16px;
   left: 16px;
   margin: 0;
-  word-spacing: 100vw;
   cursor: pointer;
+  height: 50px;
+
+  @media (max-width: 640px) {
+    height: 40px;
+  }
 `;
 
 export default () => (
   <Link href="/">
-    <Logo>Dreamer's Church</Logo>
+    <Logo src="/static/logo.svg" />
   </Link>
 );
