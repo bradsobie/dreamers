c9d6d871d235c24e343b22081517c5da2872929a
Bradley Sobie <bradsobie@gmail.com>
2019-01-01 15:37:15 -0500

    Adding sermon title and arrow

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import { Flex } from '@rebass/grid';
 import Link from 'next/link';
+import styled from 'styled-components';
 
 import AppAd from '../blocks/AppAd';
 
@@ -9,6 +10,24 @@ import Button from '../components/Button';
 import VideoBanner from '../components/VideoBanner';
 import { getHomepageData } from '../services/prismic';
 import { getLiveInfo } from '../services/churchOnline';
+
+const ArrowImage = styled.img`
+  margin-top: 16px;
+`;
+
+const LatestSermon = styled.h4`
+  text-transform: uppercase;
+  font-size: 10px;
+  color: #fff;
+  margin: 0;
+`;
+
+const SermonTitle = styled.h4`
+  font-size: 24px;
+  font-weight: 500;
+  color: #fff;
+  margin: 8px 0 16px 0;
+`;
 
 export default class extends React.Component {
   static async getInitialProps() {
@@ -36,9 +55,16 @@ export default class extends React.Component {
             containerHeight="500px"
             mobileContainerHeight="300px">
             <Flex justifyContent="center" alignItems="center" css={{ height: '100%' }}>
-              <Link href="/sermons">
-                <Button>Watch Now</Button>
-              </Link>
+              <Flex flexDirection="column" justifyContent="center" alignItems="center">
+                <LatestSermon>Latest Sermon</LatestSermon>
+                <SermonTitle>The Gift of Community</SermonTitle>
+                <Link href="/sermons">
+                  <Button>Watch Now</Button>
+                </Link>
+                <div>
+                  <ArrowImage height="12px" src="/static/arrow.svg" />
+                </div>
+              </Flex>
             </Flex>
           </VideoBanner>
         }
