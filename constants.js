858ecf09a3b0e7b711d2ed52d4f00fcc612a2701
Bradley Sobie <bradsobie@gmail.com>
2019-01-02 00:48:22 -0500

    Switching order

--- constants.js
+++ constants.js
@@ -2,8 +2,8 @@ export const NAVIGATION = [
   { text: 'About', url: '/about' },
   { text: 'Sermons', url: '/sermons' },
   { text: 'Location', url: '/location' },
-  { text: 'Giving', url: '/giving' },
-  { text: 'Next Steps', url: '/nextsteps' }
+  { text: 'Next Steps', url: '/nextsteps' },
+  { text: 'Giving', url: '/giving' }
 ];
 
 export const WATCH_NOW_LINK = 'https://dreamers-atx.churchonline.org';
