7abf0d3df85552b2cf8753304918be47476082a1
Bradley Sobie <bradsobie@gmail.com>
2019-01-01 23:17:05 -0500

    Updating map link

--- blocks/CopyrightFooter/index.js
+++ blocks/CopyrightFooter/index.js
@@ -26,7 +26,7 @@ const Info = styled.div`
 export default ({ document }) => (
   <Container justifyContent="center" alignItems="center" flexDirection="column" p={3}>
     <Info>
-      <Link href="https://goo.gl/maps/41AKmdJVAd12" target="_blank">3901 Speedway, Austin, TX 78751</Link>
+      <Link href="https://goo.gl/maps/U1Jho3buttk" target="_blank">3901 Speedway, Austin, TX 78751</Link>
       <span> | </span>
       <Link href="tel:(512) 123-1234">(512) 123-1234</Link>
     </Info>
