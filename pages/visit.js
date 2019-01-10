2365e4b05fae52e3306bcbd1e685a00c6c4ccc8e
Brad Sobie <brad_sobie@volusion.com>
2019-01-09 23:24:29 -0600

    Moving map and adding what to expect section

--- pages/visit.js
+++ pages/visit.js
@@ -51,16 +51,15 @@ export default class extends React.Component {
 
         <Container>
           <Section>
-            <h3>Time</h3>
             {RichText.render(this.props.pageProps.document.data.time)}
-          </Section>
-          
-          <Section>
-            <h3>Location</h3>
             <GoogleMap
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3444.7164380155623!2d-97.7345392848788!3d30.302133181789277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644c9af78eb1abb%3A0x2e589dab1c4c17ec!2sDreamer&#39;s+Church!5e0!3m2!1sen!2sus!4v1546407580454"
               frameborder="0"
               allowfullscreen></GoogleMap>
+          </Section>
+          
+          <Section>
+            {RichText.render(this.props.pageProps.document.data.what_to_expect)}
           </Section>
         </Container>
       </div>
