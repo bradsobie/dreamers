baa55463cb271c4d8d9eb31ae2ecfaffd4a020c8
Brad Sobie <brad_sobie@volusion.com>
2019-01-23 00:04:40 -0600

    Adding secure url

--- pages/_app.js
+++ pages/_app.js
@@ -113,6 +113,7 @@ class MyApp extends App {
           <meta property="og:site_name" content={CHURCH_NAME} />
           <meta property="og:description" content={META_DESCRIPTION} />
           <meta property="og:image" content={this.props.commonData.data.open_graph_image.url} />
+          <meta property="og:image:secure_url" content={this.props.commonData.data.open_graph_image.url} />
           <meta name="description" content={META_DESCRIPTION} />
           <meta name="keywords" content={META_KEYWORDS} />
           <link rel="shortcut icon" type="image/png" href={this.props.commonData.data.favicon.url} />
