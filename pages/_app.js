5075112120d9e75f5226230e01386dfc55a11e33
Brad Sobie <brad_sobie@volusion.com>
2019-01-23 00:12:07 -0600

    Trying secure url only

--- pages/_app.js
+++ pages/_app.js
@@ -112,7 +112,7 @@ class MyApp extends App {
           <meta property="og:url" content={SITE_URL} />
           <meta property="og:site_name" content={CHURCH_NAME} />
           <meta property="og:description" content={META_DESCRIPTION} />
-          <meta property="og:image" content={this.props.commonData.data.open_graph_image.url} />
+          {/* <meta property="og:image" content={this.props.commonData.data.open_graph_image.url} /> */}
           <meta property="og:image:secure_url" content={this.props.commonData.data.open_graph_image.url} />
           <meta name="description" content={META_DESCRIPTION} />
           <meta name="keywords" content={META_KEYWORDS} />
