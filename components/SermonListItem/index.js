63fc23d2a392a400d9acb6fbd7545345b937a292
Brad Sobie <brad_sobie@volusion.com>
2019-07-21 10:34:49 -0500

    Make sermon titles more readable

--- components/SermonListItem/index.js
+++ components/SermonListItem/index.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Link from 'next/link';
+import { Flex, Box } from '@rebass/grid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,6 +11,14 @@ const Icon = styled(FontAwesomeIcon)`
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+`;
+
+const Bar = styled(Flex).attrs({
+  alignItems: 'center',
+  p: [2, 3]
+})`
+  background-color: #333;
+  transition: background-color 0.15s;
 `;
 
 const Container = styled.a`
@@ -23,13 +32,11 @@ const Container = styled.a`
     ${Icon} {
       opacity: 1;
     }
-  }
-`;
 
-const Title = styled.span`
-  position: absolute;
-  bottom: 8px;
-  left: 8px;
+    ${Bar} {
+      background-color: #585858;
+    }
+  }
 `;
 
 const getThumbnailImage = video => video.snippet.thumbnails.maxres ? video.snippet.thumbnails.maxres.url : video.snippet.thumbnails.high.url;
@@ -39,9 +46,13 @@ const SermonListItem = ({ video }) => (
     as={`/sermon/${video.snippet.resourceId.videoId}`}
     href={`/sermon/[id]?title=${video.snippet.title}`}>
     <Container>
-      <img style={{'display': 'block'}} width="100%" src={getThumbnailImage(video)} />
-      <Title>{video.snippet.title}</Title>
-      <Icon icon={faPlayCircle} size="2x" />
+      <div style={{ position: 'relative' }}>
+        <img style={{'display': 'block'}} width="100%" src={getThumbnailImage(video)} />
+        <Icon icon={faPlayCircle} size="3x" />
+      </div>
+      <Bar>
+        <Box>{video.snippet.title}</Box>
+      </Bar>
     </Container>
   </Link>
 );
