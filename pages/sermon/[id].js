f08bdc9abe9f08339e880ed06206031d11394624
Bradley Sobie <bradsobie@gmail.com>
2019-10-19 22:40:05 -0500

    feat: add icons and links

--- pages/sermon/[id].js
+++ pages/sermon/[id].js
@@ -5,6 +5,7 @@ import { Flex, Box } from 'reflexbox';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { useMedia } from 'use-media';
 
 import PageTitle from '../../components/PageTitle';
 import Button from '../../components/Button';
@@ -26,61 +27,47 @@ const SermonTitle = styled(PageTitle)`
 const MOBILE_PLAYER_HEIGHT = '250';
 const DESKTOP_PLAYER_HEIGHT = '360';
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      videoPlayerHeight: DESKTOP_PLAYER_HEIGHT
-    };
-  }
+const Sermon = ({ pageProps, commonData, onOpenMenuClicked }) => {
+  const isWide = useMedia({ minWidth: 600 });
+  const playerHeight = isWide ? DESKTOP_PLAYER_HEIGHT : MOBILE_PLAYER_HEIGHT;
 
-  static async getInitialProps({ query }) {
-    return getVideos()
-      .then((videos) => {
-        return {
-          video: videos.find(video => video.snippet.resourceId.videoId === query.id)
-        };
-      });
-  }
-
-  componentDidMount() {
-    this.setState({
-      videoPlayerHeight: window.innerWidth > 600 ? DESKTOP_PLAYER_HEIGHT : MOBILE_PLAYER_HEIGHT
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <Head>
-          <title>{this.props.pageProps.video.snippet.title} | Dreamer's Church, Austin, TX</title>
-        </Head>
-        <VideoBanner
-          video={this.props.commonData.data.banner_video.url}
-          onOpenMenuClicked={this.props.onOpenMenuClicked}>
-          <Flex justifyContent="center" alignItems="center" css={{ height: '100%' }}>
-            <SermonTitle>{this.props.pageProps.video.snippet.title}</SermonTitle>
+  return <>
+    <Head>
+      <title>{pageProps.video.snippet.title} | Dreamer's Church, Austin, TX</title>
+    </Head>
+    <VideoBanner
+      video={commonData.data.banner_video.url}
+      onOpenMenuClicked={onOpenMenuClicked}>
+      <Flex justifyContent="center" alignItems="center" css={{ height: '100%' }}>
+        <SermonTitle>{pageProps.video.snippet.title}</SermonTitle>
+      </Flex>
+    </VideoBanner>
+    <Container>
+      <iframe
+        type="text/html"
+        width="100%"
+        height={playerHeight}
+        src={`https://www.youtube.com/embed/${pageProps.video.snippet.resourceId.videoId}?autoplay=1`}
+        frameBorder="0">
+      </iframe>
+      <p>{pageProps.video.snippet.description}</p>
+      <Link href="/sermons">
+        <Button theme="dark">
+          <Flex alignItems="center">
+            <FontAwesomeIcon icon={faChevronLeft} size="1x" />
+            <Box ml={3}>View all sermons</Box>
           </Flex>
-        </VideoBanner>
-        <Container>
-          <iframe
-            type="text/html"
-            width="100%"
-            height={this.state.videoPlayerHeight}
-            src={`https://www.youtube.com/embed/${this.props.pageProps.video.snippet.resourceId.videoId}?autoplay=1`}
-            frameBorder="0">
-          </iframe>
-          <p>{this.props.pageProps.video.snippet.description}</p>
-          <Link href="/sermons">
-            <Button theme="dark">
-              <Flex alignItems="center">
-                <FontAwesomeIcon icon={faChevronLeft} size="1x" />
-                <Box ml={3}>View all sermons</Box>
-              </Flex>
-            </Button>
-          </Link>
-        </Container>
-      </>
-    )
-  }
-}
+        </Button>
+      </Link>
+    </Container>
+  </>;
+};
+
+Sermon.getInitialProps = async ({ query }) => {
+  const videos = await getVideos();
+  return {
+    video: videos.find(video => video.snippet.resourceId.videoId === query.id)
+  };
+};
+
+export default Sermon;
--- pages/worship.js
+++ pages/worship.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
-import { Flex } from 'reflexbox';
+import { Flex, Box } from 'reflexbox';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSpotify,
+  faGooglePlay,
+  faApple
+} from '@fortawesome/free-brands-svg-icons';
 
 import PageTitle from '../components/PageTitle';
 import VideoBanner from '../components/VideoBanner';
+import useMedia from 'use-media';
 
 const Container = styled.div`
   max-width: 1000px;
@@ -12,7 +19,26 @@ const Container = styled.div`
   padding: 16px;
 `;
 
+const PlatformLink = ({ url, icon }) => (
+  <Box mx={4}>
+    <a href={url} target="_blank">
+      <FontAwesomeIcon size="3x" icon={icon} />
+    </a>
+  </Box>
+);
+
+const ListenText = styled.h3`
+  font-weight: bold;
+  margin: 0;
+`;
+
+const MOBILE_PLAYER_HEIGHT = '250';
+const DESKTOP_PLAYER_HEIGHT = '550';
+
 export default ({ commonData, onOpenMenuClicked }) => {
+  const isWide = useMedia({ minWidth: 800 });
+  const playerHeight = isWide ? DESKTOP_PLAYER_HEIGHT : MOBILE_PLAYER_HEIGHT;
+
   return (
     <>
       <Head>
@@ -30,12 +56,31 @@ export default ({ commonData, onOpenMenuClicked }) => {
         <iframe
           type="text/html"
           width="100%"
-          height="550px"
+          height={playerHeight}
           src="https://www.youtube.com/embed/272OcccA2dU"
           frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen>
         </iframe>
+
+        <Flex justifyContent="center" mt={4} mb={3}>
+          <ListenText>Listen on all digital platforms</ListenText>
+        </Flex>
+
+        <Flex justifyContent="center" alignItems="center" mt={3} mb={4}>
+          <PlatformLink
+            url="https://music.apple.com/album/1482384745?app=itunes&fbclid=IwAR0jIPcH2vM-h0tr1ZxDc9dPt_t30B1TsiiZwEm448gAW8wefyvo8O8ZVTE&ls=1"
+            icon={faApple}
+          />
+          <PlatformLink
+            url="https://play.google.com/store/music/album/Dreamer_s_Church_Miracles_Happen?id=Bk7rf4dr2mpeouruw6ym2dy3yle"
+            icon={faGooglePlay}
+          />
+          <PlatformLink
+            url="https://music.apple.com/album/1482384745?app=itunes&fbclid=IwAR0jIPcH2vM-h0tr1ZxDc9dPt_t30B1TsiiZwEm448gAW8wefyvo8O8ZVTE&ls=1"
+            icon={faSpotify}
+          />
+        </Flex>
       </Container>
     </>
   )
